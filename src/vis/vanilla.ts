79809e45ca3925fd26cf754e18308b641f21411d
psy-windschitl-lab <psy-windschitl-lab@uiowa.edu>
2026-07-28 14:42:42 -0500

    Update vanilla.ts

--- src/vis/vanilla.ts
+++ src/vis/vanilla.ts
@@ -2,4 +2,4 @@ import { createBuilderLayout } from "./layoutBuilderFactory";
 
 export const meta = { name: "vanilla" };
 
-export default createBuilderLayout({ previewMode: "live", kind: "chart" });
+export default createBuilderLayout({ previewMode: "live", kind: "chart", restrictPreviewUntilFinish: true });
